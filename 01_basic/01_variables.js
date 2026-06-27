32dcdb3f477ce0c73a7844e6030420ffe0f4a057
Aakanksha Sable <aakankshasable06@gmail.com>
2026-06-27 05:46:50 +0000

    var,let,const

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =124562
+//  const  keyword to constants 
+var accountPassword ="12453"
+// var to variable but scope probelem 
+let accountEmail ="aakanksha@22.com"
+// let for variable
+accountCity = "Bhopal"
+let accountState;
+console.log(accountId);
+accountEmail="sable12@.com"
+accountPassword ="252562"
+accountCity ="Bengaluru"
+console.table([accountEmail,accountPassword,accountCity,accountState])
