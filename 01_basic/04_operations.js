a703bd0eebd42667b3028c5f4268bff9989a8fd5
Aakanksha Sable <aakankshasable06@gmail.com>
2026-06-28 08:43:35 +0000

    operation in js

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,21 @@
+
+let str1 = "hello "
+let str2 ="JavaScript"
+let str3 =str1+str2
+console.log(str3)
+
+// console.log(1+2)
+// console.log(1-2)
+// console.log(1*2)
+// console.log(1**2)
+// console.log(1/2)
+// console.log(1%2)
+let num =33
+console.log(-num)
+
+console.log("1"+5)
+console.log("1"+"56")
+console.log(1+5+"5")
+console.log("1"+5+0)
+console.log("1"+(5+5))
+
