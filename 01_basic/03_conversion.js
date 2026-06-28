f85809df199b8dff44faec480b08f99b2dd48674
Aakanksha Sable <aakankshasable06@gmail.com>
2026-06-28 08:43:07 +0000

    conversion in javascript

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,35 @@
+let score = "45"
+let name = "Aakanksha"
+
+console.log (typeof score)
+console.log (typeof name)
+
+let numberScore = Number(score)
+let numbername = Number(name)
+
+console.log(typeof numberScore)
+console.log(typeof numbername)
+console.log( numbername)
+
+
+// "33" = 33
+//"331as" = NaN
+// true = 1,flase =0
+
+let isLoggedIn = true
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+console.log( typeof booleanisLoggedIn);
+
+// 1 or "lsk" = true
+// "" or 0 =flase
+
+let num =45
+console.log(typeof String(num))
+console.log(String(num))
+console.log(String(isLoggedIn))
+
+
+
+
