8fb1fd7b862e7470adc5f6040b2722c114abaa82
Aakanksha Sable <aakankshasable06@gmail.com>
2026-07-01 05:35:17 +0000

    math and num

---
+++ 01_basic/08_number_and_maths.js
@@ -0,0 +1,38 @@
+const score =400
+console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// // console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+const rate = 144.894
+// console.log(rate.toPrecision(3))
+
+const price = 124000000
+// console.log(price.toLocaleString("en-IN"))
+// console.log(price.valueOf())
+// console.log(price.toExponential())
+
+//          maths
+
+// console.log(Math.abs(-4512))
+// console.log(Math.round(454.465))
+// console.log(Math.floor(45.6))
+// console.log(Math.ceil(45.2))
+// console.log(Math.min(1,45,-8,5,2))
+// console.log(Math.max(1,45,-8,5,2))
+
+
+console.log(Math.random())
+const randint = (Math.random()*10)+1
+console.log(Math.round(randint))
+
+
+const min=100
+const max =300
+
+console.log(Math.floor((Math.random())*(max-min +1))+min)
