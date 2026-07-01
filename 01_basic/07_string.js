783d237e285b84a2e4f892544f602bfe9ca4bc1c
Aakanksha Sable <aakankshasable06@gmail.com>
2026-07-01 04:03:25 +0000

    string and methods

--- 01_basic/07_string.js
+++ 01_basic/07_string.js
@@ -1,4 +1,36 @@
 let name = "akanksha"
 let repocount =50
 let name2 ;
- console.log(`Hello my name is ${name2} and my repocount is ${repocount}` );
+console.log(`Hello my name is ${name} and my repocount is ${repocount}` );
+
+const beautyname = new String("Stars in the Sky")
+
+// console.log(beautyname[0])
+// console.log(beautyname.__proto__)
+// console.log(beautyname.length)
+// console.log(beautyname.toUpperCase())
+// console.log(beautyname.toLowerCase())
+// console.log(beautyname.charAt(5))
+
+// console.log(beautyname.indexOf("p"))
+// const  v = beautyname.slice(1,5)
+// console.log(v)
+// console.log(beautyname.substring(0,6))
+// const url ="https://google.com/t%20"
+// console.log(url.replace("%20","/"))
+
+// console.log(url.includes("%200"))
+
+// const words = beautyname.split(" ")
+// console.log(words)
+console.log(beautyname.at(2))
+console.log(beautyname.codePointAt(2))
+console.log(beautyname.concat(" and the Moon is the asethic"))
+console.log(beautyname.endsWith("asethic"))
+console.log(beautyname.lastIndexOf("t"))
+console.log(beautyname.isWellFormed())
+
+
+
+
+
