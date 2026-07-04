486bed7b19d15dbb9025739f08a5aadd66e48111
Aakanksha Sable <aakankshasable06@gmail.com>
2026-07-04 01:41:50 +0000

    de-structure

---
+++ 02_basic/05_object.js
@@ -0,0 +1,9 @@
+ const course ={
+    coursename :"javaScript",
+    fees :"1000",
+    courseInstructor :"hitesh"
+ }
+
+ const { courseInstructor : instructor} =course
+ console.log(instructor);
+ 
