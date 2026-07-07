aa68df62f92167c87080a721a8ba15a11e1c71cc
Aakanksha Sable <aakankshasable06@gmail.com>
2026-07-07 10:00:23 +0000

    this and arrow function

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -9,9 +9,25 @@ const user ={
 
 }
 
-// user.welcomeMessage()
-// user.username="sam"
-// user.welcomeMessage()
+user.welcomeMessage()
+user.username="sam"
+user.welcomeMessage()
 
 console.log(this);
 
+const chai = () =>{
+    let username ="chai"
+    console.log(this)
+}
+
+chai()
+// const addtwo = (num1,num2) =>{
+//     return num1+num2
+// }
+// console.log(addtwo(5,8));
+
+
+// const addtwo = (num1,num2) => num1+num2
+const addtwo = (num1,num2) => (num1+num2)
+
+console.log(addtwo(5,8));
