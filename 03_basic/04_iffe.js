68664ae5495e80429f9ad22b8fce05f363487a5e
Aakanksha Sable <aakankshasable06@gmail.com>
2026-07-07 10:36:05 +0000

    iffe

---
+++ 03_basic/04_iffe.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIFE)
+(function one (){
+    console.log("DB Connected");   
+})();
+
+((name) => {
+    console.log("db connectedd"+ name );
+    
+    
+})("sam")
